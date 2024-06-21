9dd0bc69f9a71bb6b0db1e546956cc98f659da33
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2024-06-21 17:22:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,24 @@
 (function(channelNameSize){
   const mapping=[], typedarray=new Uint8Array(channelNameSize), map=new Map();
   for(let i=0;i<256;i++) mapping[i]=String.fromCharCode(i);
-  function randomChannel(){
-    let arr=crypto.getRandomValues(typedarray), str="";
-    for(let i=0;i<arr.length;i++) str+=mapping[arr[i]];
+  function randomChannel(input=true){
+    do{
+      var arr=crypto.getRandomValues(typedarray), str="";
+      for(let i=0;i<arr.length;i++) str+=mapping[arr[i]];
+    }while(map.has(str));
+    map.set(str,input);
     return str;
   }
-  const channel=
+  const channel=randomChannel(), sender=new BroadcastChannel(channel), receiver=new BroadcastChannel(channel);
+  map.delete(channel); //it isn't needed to be unique towards other map entries
+  receiver.addEventListener('message',function(){
+    map.forEach(function(timer,ID){
+      if(performance.now()-timer.start >= timer.ms) entry.userFN();
+      if(!entry.repeat) map.remove(ID);
+    })
+  })
   function timeout(userFN,ms){
-    
+    return randomChannel({userFN,ms,repeat:false,start:performance.now()});
   }
+  window.timer={timeout} //other functions added soon
 })(16)
