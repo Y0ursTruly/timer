f5ef82637b4daac82330a16c16a241823bd8b98e
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2024-06-28 02:26:22 +0000

    timeouts (timeout and interval) now return integers instead of strings (the performance boost in setting and clearing is crazy)

--- docile/alternate_timer.js
+++ docile/alternate_timer.js
@@ -2,37 +2,61 @@
 //look at the difference when this was used as the timer.js: https://github.com/Y0ursTruly/timer/blob/de690997b914a6ccb7cfc612a75c89ed843ecd70/image.png
 //compared to the current timer.js: https://github.com/Y0ursTruly/timer/blob/bc35b4230943b66b60cdd719869852b7c3d2d377/image.png
 (function(channelNameSize){
+
+  class Queue{
+    //each node is in form [data,next]
+    top=null;
+    end=null;
+    constructor(){}
+    add(data){
+      const node=[data,null];
+      this.top? (this.end[1]=node,this.end=node): (this.top=node,this.end=node);
+    }
+    pop(){
+      if(!this.top) return null;
+      const [data,next]=this.top;
+      return (this.top=next,data);
+    }
+    empty(){return !this.top}
+  }
+  const timers={__proto__:null,length:0,count:0,queue:new Queue()};
+
   const crypto=typeof window==="undefined"?require('node:crypto'):window.crypto;
-  const mapping=[], typedarray=new Uint8Array(channelNameSize), map=new Map();
+  const mapping=[], typedarray=new Uint8Array(channelNameSize);
   for(let i=0;i<256;i++) mapping[i]=String.fromCharCode(i);
-  function randomChannel(input=true){
-    do{
-      var arr=crypto.getRandomValues(typedarray), str="";
-      for(let i=0;i<arr.length;i++) str+=mapping[arr[i]];
-    }while(map.has(str));
-    map.set(str,input);
+  function randomChannel(input){
+    if(input){
+      const num=timers.queue.empty()? timers.length++: timers.queue.pop();
+      return (timers[num]=input),timers.count++,num;
+    }
+    //below is support in same function but for cryptographically secure random channel name
+    var arr=crypto.getRandomValues(typedarray), str="";
+    for(let i=0;i<arr.length;i++) str+=mapping[arr[i]];
     return str;
   }
 
   //timer engine begin
   let channel=randomChannel(), sender=null, receiver=null, curr=Math.floor(performance.now());
-  map.delete(channel); //it isn't needed to be unique towards other map entries
-  function manageTimer(timer,ID){
-    //manages each timer in the map of timers
-    if(curr-timer.start >= timer.ms){
-      timer.userFN();
-      if(!timer.repeat) map.delete(ID);
-      else timer.start=curr;
-    }
-  }
   function listener(){
-    if(!map.size) return deactivate();
+    if(!timers.count) return deactivate();
     sender.postMessage(null); //repeat the channel messaging IF TIMER(S) EXIST
     //the idea here is something that must be waited on but doesn't resolve quickly enough to hang the process
     const now=performance.now();
     if(now-curr<1) return null;
     curr=Math.floor(now);
-    map.forEach(manageTimer);
+    for(let i=0;i<timers.length;i++){
+      if(!timers[i]) continue;
+      const timer=timers[i];
+      if(curr-timer.start >= timer.ms){
+        timer.userFN();
+        if(!timer.repeat){
+          delete timers[i];
+          timers.queue.add(i);
+          timers.count--;
+        }
+        else timer.start=curr;
+      }
+    }
   }
   let active=false;
   function activate(){
@@ -51,7 +75,7 @@
     active=false;
   }
   //timer engine end
-  
+
   function timeout(userFN,ms){
     activate();
     return randomChannel({userFN,ms,repeat:false,start:performance.now()});
@@ -65,7 +89,13 @@
     timeout(resolver,ms);
     return await promise;
   }
-  function clear(ID){return map.delete(ID)}
+  function clear(ID){
+    if(!timers[ID]) return false;
+    delete timers[ID];
+    timers.queue.add(ID);
+    timers.count--;
+    return true;
+  }
 
 
   //exports
--- timer.js
+++ timer.js
@@ -1,33 +1,56 @@
 (function(channelNameSize){
+
+  class Queue{
+    //each node is in form [data,next]
+    top=null;
+    end=null;
+    constructor(){}
+    add(data){
+      const node=[data,null];
+      this.top? (this.end[1]=node,this.end=node): (this.top=node,this.end=node);
+    }
+    pop(){
+      if(!this.top) return null;
+      const [data,next]=this.top;
+      return (this.top=next,data);
+    }
+    empty(){return !this.top}
+  }
+  const timers={__proto__:null,length:0,count:0,queue:new Queue()};
+
   const crypto=typeof window==="undefined"?require('node:crypto'):window.crypto;
-  const mapping=[], typedarray=new Uint8Array(channelNameSize), map=new Map();
+  const mapping=[], typedarray=new Uint8Array(channelNameSize);
   for(let i=0;i<256;i++) mapping[i]=String.fromCharCode(i);
-  function randomChannel(input=true){
-    do{
-      var arr=crypto.getRandomValues(typedarray), str="";
-      for(let i=0;i<arr.length;i++) str+=mapping[arr[i]];
-    }while(map.has(str));
-    map.set(str,input);
+  function randomChannel(input){
+    if(input){
+      const num=timers.queue.empty()? timers.length++: timers.queue.pop();
+      return (timers[num]=input),timers.count++,num;
+    }
+    //below is support in same function but for cryptographically secure random channel name
+    var arr=crypto.getRandomValues(typedarray), str="";
+    for(let i=0;i<arr.length;i++) str+=mapping[arr[i]];
     return str;
   }
 
   //timer engine begin
   let channel=randomChannel(), sender=null, receiver=null;
-  map.delete(channel); //it isn't needed to be unique towards other map entries
-  function manageTimer(timer,ID){
-    //manages each timer in the map of timers
-    const now=performance.now()
-    if(now-timer.start >= timer.ms){
-      timer.userFN();
-      if(!timer.repeat) map.delete(ID);
-      else timer.start=now;
-    }
-  }
   function listener(){
-    if(!map.size) return deactivate();
+    if(!timers.count) return deactivate();
     sender.postMessage(null); //repeat the channel messaging IF TIMER(S) EXIST
     //the idea here is something that must be waited on but doesn't resolve quickly enough to hang the process
-    map.forEach(manageTimer);
+    for(let i=0;i<timers.length;i++){
+      if(!timers[i]) continue;
+      const now=performance.now(), timer=timers[i];
+      if(now-timer.start >= timer.ms){
+        timer.userFN();
+        if(!timer.repeat){
+          delete timers[i];
+          timers.queue.add(i);
+          timers.count--;
+        }
+        else timer.start=now;
+      }
+    }
   }
   let active=false;
   function activate(){
@@ -46,7 +69,7 @@
     active=false;
   }
   //timer engine end
-  
+
   function timeout(userFN,ms){
     activate();
     return randomChannel({userFN,ms,repeat:false,start:performance.now()});
@@ -60,7 +83,13 @@
     timeout(resolver,ms);
     return await promise;
   }
-  function clear(ID){return map.delete(ID)}
+  function clear(ID){
+    if(!timers[ID]) return false;
+    delete timers[ID];
+    timers.queue.add(ID);
+    timers.count--;
+    return true;
+  }
 
 
   //exports
